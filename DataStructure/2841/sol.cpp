cd52185a4eb4269b6ba4a471e397042bc94da25d
Jack-R-lantern <tjdfkr2421@gmail.com>
2020-12-02 20:38:41 +0900

    ps_20201202

---
+++ DataStructure/2841/sol.cpp
@@ -0,0 +1,30 @@
+#include <cstdio>
+#include <vector>
+using namespace std;
+
+int		main(void) {
+	int		N, P;
+	int		i, val;
+	int		cnt = 0;
+	scanf("%d %d", &N, &P);
+	vector<vector<int>>	v(N);
+	while (N--) {
+		scanf("%d %d", &i, &val);
+		if(v[i-1].empty()) {
+			v[i-1].push_back(val);
+			cnt++;
+		}
+		else {
+			while (v[i-1].back() > val) {
+				cnt++;
+				v[i-1].pop_back();
+			}
+			if (v[i-1].back() < val) {
+				v[i-1].push_back(val);
+				cnt++;
+			}
+		}
+	}
+	printf("%d\n", cnt);
+	return (0);
+}
