c1da7fcbdcd6297e8d5893cd64e975770b7ed699
Jack-R-lantern <tjdfkr2421@gmail.com>
2020-12-09 05:00:24 +0900

    ps_20201209

--- DataStructure/2304/sol.cpp
+++ DataStructure/2304/sol.cpp
@@ -6,8 +6,8 @@ using namespace std;
 deque<pair<int, int>>	dq;
 
 int		main(void) {
-	int		N;
-	int		L, H;
+	int		N, total = 0;
+	int		L, H, R, RH;
 	int		max = 0;
 
 	scanf("%d", &N);
@@ -17,8 +17,33 @@ int		main(void) {
 		dq.push_back({L, H});
 	}
 	sort(dq.begin(), dq.end());
-	while (dq.front().second != max) {
-		
+	L = dq.front().first;
+	H = dq.front().second;
+	if(H < max)
+		dq.pop_front();
+	while (H < max) {
+		if (H < dq.front().second) {
+			total += (dq.front().first - L) * H;
+			H = dq.front().second;
+			L = dq.front().first;
+		}
+		if(dq.front().second < max)
+			dq.pop_front();
 	}
+	R = dq.back().first;
+	RH = dq.back().second;
+	if (RH < max)
+		dq.pop_back();
+	while (RH < max) {
+		if (RH < dq.back().second) {
+			total += abs(dq.back().first - R) * RH;
+			RH = dq.back().second;
+			R = dq.back().first;
+		}
+		if(dq.back().second < max)
+			dq.pop_back();
+	}
+	total += (abs(dq.front().first - dq.back().first) + 1) * max;
+	printf("%d\n", total);
 	return (0);
 }
