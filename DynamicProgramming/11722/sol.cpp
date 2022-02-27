adcf355bc43e7239f38397214f0871ba447b8cd5
Jack-R-lantern <tjdfkr2421@gmail.com>
2022-02-27 18:39:16 +0900

    ps_20220227

---
+++ DynamicProgramming/11722/sol.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int N, MAX;
+int A[1001];
+int dp[1001];
+
+int		main(void) {
+	cin >> N;
+	for (int i=1;i<=N;i++)
+		cin >> A[i];
+	for (int i=1;i<=N;i++) {
+		dp[i] = 1;
+		for (int j=1;j<i;j++) {
+			if (A[i] < A[j] && dp[i] < dp[j] + 1)
+				dp[i] = dp[j] + 1;
+		}
+		if (MAX < dp[i])
+			MAX = dp[i];
+	}
+	cout << MAX << endl;
+	return 0;
+}
