12bd543f6ce45d1d98bb47826c7e3ab7d3283924
Jack-R-lantern <tjdfkr2421@gmail.com>
2022-05-09 23:24:51 +0900

    ps_20220509

---
+++ BinarySearch/2417/sol.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+long long binarySearch(long long l, long long r, long long n) {
+	if(r >= l) {
+		long long mid = l + (r - l) / 2;
+		if(mid  == sqrtl(n)) {
+			return mid;
+		}
+		if(mid > sqrtl(n)) {
+			return binarySearch(l, mid - 1, n);
+		}
+		return binarySearch(mid + 1, r, n);
+	}
+	return l;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	long long n;
+	cin >> n;
+	cout << binarySearch(0, n, n);
+
+	return 0;
+}
